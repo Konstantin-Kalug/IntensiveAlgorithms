4f62756ae6d920efe4bcac43f2848224c2bd648e
Konstantin_Kalug <nl17nl17@outlook.com>
2023-08-18 20:43:37 +0300

    Binary search

--- BinarySearch/binary_search.py
+++ BinarySearch/binary_search.py
@@ -2,18 +2,17 @@ from typing import List
 
 
 def binary_search(value: int, array: List[int]) -> int:
-    """
-    Returns index of value in ascending sorted array
-
-    Args:
-        value (int): value to find position of
-        array (List[int]): list of sorted numbers
-
-    Returns:
-        int: index of value
-    """
-
-    return 0
+    l, r = 0, len(array)
+    while l < r:
+        m = (l + r + 1) // 2
+        if array[m] > value:
+            r = m - 1
+        elif array[m] < value:
+            l = m
+        else:
+            l = m
+            break
+    return l - 1
 
 
 # https://leetcode.com/problems/binary-search/
