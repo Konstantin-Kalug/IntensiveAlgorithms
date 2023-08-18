835e85aab1794acb898be9d8e9876aa5616af7b3
Konstantin_Kalug <nl17nl17@outlook.com>
2023-08-18 18:42:33 +0300

    insertion sort

--- InsertionSort/insertion_sort.py
+++ InsertionSort/insertion_sort.py
@@ -2,17 +2,12 @@ from typing import List
 
 
 def insertion_sort(array: List[int]) -> List[int]:
-    """
-    Sorts the array in ascending order using insertion sort
-
-    Args:
-        array (List[int]): input
-
-    Returns:
-        List[int]: sorted array
-    """
-
-    return []
+    for i in range(1, len(array)):
+        j = i
+        while j != 0 and array[j] < array[j - 1]:
+            array[j], array[j - 1] = array[j - 1], array[j]
+            j -= 1
+    return array
 
 
 # https://leetcode.com/problems/sort-colors/
