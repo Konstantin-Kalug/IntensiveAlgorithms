be2b3ab6770d64befc72914046904f9195957378
Konstantin_Kalug <nl17nl17@outlook.com>
2023-08-18 22:06:51 +0300

    prime

--- PrimeNumbers/prime_numbers.py
+++ PrimeNumbers/prime_numbers.py
@@ -2,17 +2,21 @@ from typing import List
 
 
 def prime_numbers(n: int) -> List[int]:
-    """
-    Return all prime numbers from 1 to n (including)
-
-    Args:
-        n (int): number
-
-    Returns:
-        List[int]: prime numbers
-    """
-
-    return []
+    return
 
 
 # https://leetcode.com/problems/count-primes/
+def prime_numbers(n: int) -> List[int]:
+    array = [i for i in range(2, n)]
+    num = 2
+    j = 0
+    while num <= int(n ** 0.5):
+        i = j + 1
+        while i != len(array):
+            if array[i] % num == 0 and array[i] != num:
+                del array[i]
+            else:
+                i += 1
+        j += 1
+        num = array[j]
+    return len(array)
