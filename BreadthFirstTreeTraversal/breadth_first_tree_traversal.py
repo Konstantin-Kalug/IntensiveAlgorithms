1b73e64961ca25fd6d8c0620bc68810cf18a5c7e
Konstantin_Kalug <nl17nl17@outlook.com>
2023-08-18 20:18:09 +0300

    BFS

--- BreadthFirstTreeTraversal/breadth_first_tree_traversal.py
+++ BreadthFirstTreeTraversal/breadth_first_tree_traversal.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Optional
 from typing import List
 
 
@@ -17,3 +17,41 @@ def breadth_first_tree_traversal(root: int, tree: Dict[int, List[int]]) -> List[
 
 
 # https://leetcode.com/problems/binary-tree-level-order-traversal/
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        output = []
+        self.bfs(root, output, 0)
+        ans = []
+        output = sorted(output, key=lambda x: x[1])
+        for j in range(len(output)):
+            if output[j][1] == len(ans) - 1:
+                ans[-1] += output[j][0]
+            else:
+                ans.append(output[j][0])
+        return ans
+
+    def bfs(self, root, output, i):
+        if root is None:
+            return
+        if len(output) == 0:
+            output.append(([root.val], i))
+            i += 1
+        preoutput = []
+        if root.left is not None:
+            preoutput.append(root.left.val)
+        if root.right is not None:
+            preoutput.append(root.right.val)
+        if len(preoutput) != 0:
+            output.append((preoutput, i))
+        if root.left is not None:
+            self.bfs(root.left, output, i + 1)
+        if root.right is not None:
+            self.bfs(root.right, output, i + 1)
