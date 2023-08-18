947591a08d5386acc219488caa0ae15cf279abb3
Konstantin_Kalug <nl17nl17@outlook.com>
2023-08-18 19:49:44 +0300

    DFS

--- DepthFirstTreeTraversal/depth_first_tree_traversal.py
+++ DepthFirstTreeTraversal/depth_first_tree_traversal.py
@@ -17,3 +17,48 @@ def dfs(output, root, tree):
 
 
 # https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        new_tree = {}
+        if k == 0:
+            return [target.val]
+        point = int(target.val)
+        self.dfs1(root, new_tree)
+        points = [target.val]
+        output = []
+        self.dfs2(new_tree, target.val, 0, k, points, output)
+        return sorted(output, key=lambda x: -x)
+
+    def dfs1(self, root, new_tree):
+        if root.val is None:
+            return
+        if root.val not in new_tree:
+            new_tree[root.val] = []
+        if not (root.left is None):
+            new_tree[root.val] += [root.left.val]
+            new_tree[root.left.val] = [root.val]
+        if not (root.right is None):
+            new_tree[root.val] += [root.right.val]
+            new_tree[root.right.val] = [root.val]
+        if not (root.left is None):
+            self.dfs1(root.left, new_tree)
+        if not (root.right is None):
+            self.dfs1(root.right, new_tree)
+
+    def dfs2(self, tree, t, n, k, points, output):
+        for r in tree[t]:
+            if r not in points and not (r is None):
+                points.append(r)
+                if n + 1 == k:
+                    output.append(r)
+                else:
+                    self.dfs2(tree, r, n + 1, k, points, output)
